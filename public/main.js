593e723f34c6ade5096ddf2b7536545a91a6cf7d
Erika Stigleman <Erika.Stigleman@outlook.com>
2021-02-23 20:30:10 -0500

    if logic complete

--- public/main.js
+++ public/main.js
@@ -1,6 +1,48 @@
 function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  const player1choice = ''
+
+  const player2choice = ''
+
+  const winner = () => {
+    if (player1choice !== '' && player2choice !== '') {
+      let footer = document.querySelector('footer')
+
+      if (player1choice === 'paper' && player2choice === 'paper') {
+        footer.textContent = 'Draw!'
+      }
+
+      if (player1choice === 'rock' && player2choice === 'rock') {
+        footer.textContent = 'Draw!'
+      }
+
+      if (player1choice === 'scissors' && player2choice === 'scissors') {
+        footer.textContent = 'Draw!'
+      }
+
+      if (player1choice === 'scissors' && player2choice === 'rock') {
+        footer.textContent = 'Player 2 WINS!'
+      }
+
+      if (player1choice === 'scissors' && player2choice === 'paper') {
+        footer.textContent = 'Player 1 WINS!'
+      }
+
+      if (player1choice === 'rock' && player2choice === 'scissors') {
+        footer.textContent = 'Player 1 WINS!'
+      }
+
+      if (player1choice === 'rock' && player2choice === 'paper') {
+        footer.textContent = 'Player 2 WINS!'
+      }
+
+      if (player1choice === 'paper' && player2choice === 'scissors') {
+        footer.textContent = 'Player 2 WINS!'
+      }
+
+      if (player1choice === 'paper' && player2choice === 'rock') {
+        footer.textContent = 'Player 1 WINS!'
+      }
+    }
   }
 }
 
